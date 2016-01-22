61680702221c1ad8d6a3e781f19d9e695730511f
Markus <fleckmarkus.privat@googlemail.com>
2016-01-22 15:17:17 +0100

    Add more example stream implementations

---
+++ Scripts/LSLMarkerStream.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LSLMarkerStream : MonoBehaviour {
+    
+    private const string unique_source_id = "D3F83BB699EB49AB94A9FA44B88882AB";
+    
+    public string lslStreamName = "Unity_<Paradigma_Name_here>";
+	public string lslStreamType = "LSL_Marker_Strings";
+    
+	private LSL.liblsl.StreamInfo lslStreamInfo;
+	private LSL.liblsl.StreamOutlet lslOutlet;
+	private int lslChannelCount = 1;
+	private double nominalRate = 0;
+	private const LSL.liblsl.channel_format_t lslChannelFormat = LSL.liblsl.channel_format_t.cf_string;
+
+	private string[] sample; 
+
+	// Use this for initialization
+	void Start () {
+		sample = new string[lslChannelCount];
+
+		lslStreamInfo = new LSL.liblsl.StreamInfo(
+				lslStreamName,
+				lslStreamType,
+				lslChannelCount,
+				nominalRate,
+				lslChannelFormat,
+                unique_source_id);
+
+		lslOutlet = new LSL.liblsl.StreamOutlet(lslStreamInfo);
+	}
+	
+	public void Write(string marker, float customTimeStamp)
+	{
+		sample[0] = marker;
+		lslOutlet.push_sample(sample, customTimeStamp); 
+    }
+
+    private string pendingMarker;
+
+    public void WriteBeforeFrameIsDisplayed(string marker)
+    {
+        pendingMarker = marker;
+        StartCoroutine(WriteMarkerAfterImageIsRendered());
+    }
+
+    IEnumerator WriteMarkerAfterImageIsRendered()
+    {
+        yield return new WaitForEndOfFrame();
+        
+        lslOutlet.push_sample(sample);
+
+        yield return null;
+    }
+
+}
---
+++ Scripts/LSLTransformOutlet.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using LSL;
+
+/// <summary>
+/// An reusable example of an outlet which provides the orientation of an entity to LSL
+/// </summary>
+public class LSLTransformOutlet : MonoBehaviour {
+
+    private const string unique_source_id = "63CE5B03731944F6AC30DBB04B451A94";
+
+    private liblsl.StreamOutlet outlet;
+    private liblsl.StreamInfo streamInfo;
+    
+    /// <summary>
+    /// Use a array to reduce allocation costs
+    /// </summary>
+    private float[] currentSample;
+
+    public string StreamName = "BeMoBI.Unity.Orientation.<Add_a_entity_id_here>";
+    public string StreamType = "Unity.Quarternion";
+    public int ChannelCount = 4;
+
+    public Transform sampleSource;
+
+    void Start()
+    {
+        // initialize the array once
+        currentSample = new float[ChannelCount];
+
+        streamInfo = new liblsl.StreamInfo(StreamName, StreamType, ChannelCount, Time.fixedDeltaTime, liblsl.channel_format_t.cf_float32, unique_source_id);
+
+        outlet = new liblsl.StreamOutlet(streamInfo);
+    }
+
+    void LateUpdate()
+    {
+        if (outlet == null)
+            return;
+
+        var rotation = sampleSource.rotation;
+
+        // reuse the array for each sample to reduce allocation costs
+        currentSample[0] = rotation.x;
+        currentSample[1] = rotation.y;
+        currentSample[2] = rotation.z;
+        currentSample[3] = rotation.w;
+
+        outlet.push_sample(currentSample, LSLTimeSync.Instance.UpdateTimeStamp);
+    }
+}
