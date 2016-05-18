b217652bef96d8f8ca414ddf558ef4b9890f760d
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-05-18 18:32:04 +0200

    Remove experimental components from master branch and introducing namespaces

--- Editor/LSLEditorIntegration.cs
+++ Editor/LSLEditorIntegration.cs
@@ -1,43 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEditor;
 using System.IO;
 
-public class LSLEditorIntegration : ScriptableObject
+namespace Assets.LSL4Unity.Editor
 {
-    static readonly string wikiURL = "https://github.com/xfleckx/LSL4Unity/wiki";
-    static readonly string lib64Name = "liblsl64.dll";
-    static readonly string lib32Name = "liblsl32.dll";
-    static readonly string wrapperFileName = "LSL.cs";
-    static readonly string assetSubFolder = "LSL4Unity";
-    static readonly string libFolder = assetSubFolder + @"/lib"; 
-
-    [MenuItem("LSL/LSL Window")]
-    static void OpenLSLWindow()
+    public class LSLEditorIntegration : ScriptableObject
     {
-        LSLEditor window = (LSLEditor)EditorWindow.GetWindow(typeof(LSLEditor));
-        window.Init();
-        
+        static readonly string wikiURL = "https://github.com/xfleckx/LSL4Unity/wiki";
+        static readonly string lib64Name = "liblsl64.dll";
+        static readonly string lib32Name = "liblsl32.dll";
+        static readonly string wrapperFileName = "LSL.cs";
+        static readonly string assetSubFolder = "LSL4Unity";
+        static readonly string libFolder = assetSubFolder + @"/lib";
+
+        [MenuItem("LSL/LSL Window")]
+        static void OpenLSLWindow()
+        {
+            LSLEditor window = (LSLEditor)EditorWindow.GetWindow(typeof(LSLEditor));
+            window.Init();
+
+        }
+
+        [MenuItem("LSL/LSL Window", true)]
+        static bool ValidateOpenLSLWindow()
+        {
+            string root = Application.dataPath;
+
+            bool lib64Available = false;
+            bool lib32Available = false;
+            bool apiAvailable = false;
+
+            lib32Available = File.Exists(Path.Combine(root, Path.Combine(libFolder, lib32Name)));
+            lib64Available = File.Exists(Path.Combine(root, Path.Combine(libFolder, lib64Name)));
+            apiAvailable = File.Exists(Path.Combine(root, Path.Combine(assetSubFolder, wrapperFileName)));
+
+            if ((lib64Available || lib32Available) && apiAvailable)
+                return true;
+
+            Debug.LogError("LabStreamingLayer libraries not available! See " + wikiURL + " for installation instructions");
+            return false;
+        }
+
     }
-
-    [MenuItem("LSL/LSL Window", true)]
-    static bool ValidateOpenLSLWindow()
-    {
-        string root = Application.dataPath;
-
-        bool lib64Available = false; 
-        bool lib32Available = false;
-        bool apiAvailable = false;
-
-        lib32Available = File.Exists(Path.Combine(root, Path.Combine(libFolder, lib32Name)));
-        lib64Available = File.Exists(Path.Combine(root, Path.Combine(libFolder, lib64Name)));
-        apiAvailable = File.Exists(Path.Combine(root, Path.Combine(assetSubFolder, wrapperFileName)));
-
-        if ((lib64Available || lib32Available) && apiAvailable)
-            return true;
-        
-        Debug.LogError("LabStreamingLayer libraries not available! See " + wikiURL + " for installation instructions");
-        return false;
-    }
-
 }
--- Editor/LSLEditorWindow.cs
+++ Editor/LSLEditorWindow.cs
@@ -2,115 +2,111 @@
 using UnityEditor;
 using LSL;
 using System.Collections.Generic;
-using Assets.LSL4Unity.Editor;
 
-public class LSLEditor : EditorWindow
+namespace Assets.LSL4Unity.Editor
 {
-    public double WaitOnResolveStreams = 2;
-
-    private const string noStreamsFound = "No streams found!";
-    private const string clickLookUpFirst = "Click lookup first";
-    private const string nStreamsFound = " Streams found";
-
-    private List<string> listNamesOfStreams = new List<string>();
-
-    private Vector2 scrollVector;
-    private string streamLookUpResult;
-
-    private liblsl.ContinuousResolver resolver;
-
-    public void Init()
+    public class LSLEditor : EditorWindow
     {
-        resolver = new liblsl.ContinuousResolver();
+        public double WaitOnResolveStreams = 2;
 
-        this.titleContent = new GUIContent("LSL Utility");
-    }
+        private const string noStreamsFound = "No streams found!";
+        private const string clickLookUpFirst = "Click lookup first";
+        private const string nStreamsFound = " Streams found";
 
-    liblsl.StreamInfo[] streamInfos = null;
+        private List<string> listNamesOfStreams = new List<string>();
 
-    void OnGUI()
-    {
-        if (resolver == null)
-            Init();
+        private Vector2 scrollVector;
+        private string streamLookUpResult;
 
-        EditorGUILayout.BeginVertical();
-        EditorGUILayout.BeginHorizontal();
+        private liblsl.ContinuousResolver resolver;
 
-        UpdateStreams();
-
-        EditorGUILayout.LabelField(streamLookUpResult, EditorStyles.boldLabel);
-
-        EditorGUILayout.EndHorizontal();
-
-        EditorGUILayout.Space();
-        EditorGUILayout.Separator();
-
-        scrollVector = EditorGUILayout.BeginScrollView(scrollVector, GUILayout.Width(EditorGUIUtility.currentViewWidth), GUILayout.Height(150));
-        GUILayoutOption fieldWidth = GUILayout.Width(EditorGUIUtility.currentViewWidth / 4.3f);
-        // GUILayoutOption[] parameter = new GUILayoutOption[]{ };
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.Space();
-        EditorGUILayout.LabelField("Name", EditorStyles.boldLabel, fieldWidth);
-        EditorGUILayout.LabelField("Type", EditorStyles.boldLabel, fieldWidth);
-        EditorGUILayout.LabelField("HostName", EditorStyles.boldLabel, fieldWidth);
-        EditorGUILayout.LabelField("Data Rate", EditorStyles.boldLabel, fieldWidth);
-        EditorGUILayout.EndHorizontal();
-
-        foreach (var item in listNamesOfStreams)
+        public void Init()
         {
-            string[] s = item.Split(' ');
+            resolver = new liblsl.ContinuousResolver();
 
+            this.titleContent = new GUIContent("LSL Utility");
+        }
+
+        liblsl.StreamInfo[] streamInfos = null;
+
+        void OnGUI()
+        {
+            if (resolver == null)
+                Init();
+
+            EditorGUILayout.BeginVertical();
             EditorGUILayout.BeginHorizontal();
-             
-            EditorGUILayout.LabelField(new GUIContent(s[0], s[0]), fieldWidth);
-            EditorGUILayout.LabelField(new GUIContent(s[1], s[1]), fieldWidth);
-            EditorGUILayout.LabelField(new GUIContent(s[2], s[2]), fieldWidth);
-            EditorGUILayout.LabelField(new GUIContent(s[3], s[3]), fieldWidth);
-            
-            if (GUILayout.Button("Visualize"))
-            {
-                var visualWindow = StreamVisualWindow.GetNewInstanceFor(s[0]);
 
-                visualWindow.Show();
-            }
+            UpdateStreams();
+
+            EditorGUILayout.LabelField(streamLookUpResult, EditorStyles.boldLabel);
 
             EditorGUILayout.EndHorizontal();
 
-            if (GUILayoutUtility.GetLastRect().Contains(Event.current.mousePosition))
-            {
-                if (Event.current.type == EventType.MouseUp)
-                {
-                    Debug.Log("Mouse click on :" + item);
+            EditorGUILayout.Space();
+            EditorGUILayout.Separator();
 
-                    
+            scrollVector = EditorGUILayout.BeginScrollView(scrollVector, GUILayout.Width(EditorGUIUtility.currentViewWidth), GUILayout.Height(150));
+            GUILayoutOption fieldWidth = GUILayout.Width(EditorGUIUtility.currentViewWidth / 4.3f);
+            // GUILayoutOption[] parameter = new GUILayoutOption[]{ };
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Name", EditorStyles.boldLabel, fieldWidth);
+            EditorGUILayout.LabelField("Type", EditorStyles.boldLabel, fieldWidth);
+            EditorGUILayout.LabelField("HostName", EditorStyles.boldLabel, fieldWidth);
+            EditorGUILayout.LabelField("Data Rate", EditorStyles.boldLabel, fieldWidth);
+            EditorGUILayout.EndHorizontal();
+
+            foreach (var item in listNamesOfStreams)
+            {
+                string[] s = item.Split(' ');
+
+                EditorGUILayout.BeginHorizontal();
+
+                EditorGUILayout.LabelField(new GUIContent(s[0], s[0]), fieldWidth);
+                EditorGUILayout.LabelField(new GUIContent(s[1], s[1]), fieldWidth);
+                EditorGUILayout.LabelField(new GUIContent(s[2], s[2]), fieldWidth);
+                EditorGUILayout.LabelField(new GUIContent(s[3], s[3]), fieldWidth);
+
+
+                EditorGUILayout.EndHorizontal();
+
+                if (GUILayoutUtility.GetLastRect().Contains(Event.current.mousePosition))
+                {
+                    if (Event.current.type == EventType.MouseUp)
+                    {
+                        Debug.Log("Mouse click on :" + item);
+
+
+                    }
                 }
             }
+            EditorGUILayout.EndScrollView();
+            EditorGUILayout.EndVertical();
+
         }
-        EditorGUILayout.EndScrollView();
-        EditorGUILayout.EndVertical();
 
-    }
-
-    private void UpdateStreams()
-    {
-        listNamesOfStreams.Clear();
-
-        streamInfos = resolver.results();
-
-        if (streamInfos.Length == 0)
+        private void UpdateStreams()
         {
-            Debug.LogWarning(noStreamsFound);
-            streamLookUpResult = noStreamsFound;
-        }
-        else
-        {
-            foreach (var item in streamInfos)
+            listNamesOfStreams.Clear();
+
+            streamInfos = resolver.results();
+
+            if (streamInfos.Length == 0)
             {
-                listNamesOfStreams.Add(string.Format("{0} {1} {2} {3}", item.name(), item.type(), item.hostname(), item.nominal_srate()));
+                streamLookUpResult = noStreamsFound;
+            }
+            else
+            {
+                foreach (var item in streamInfos)
+                {
+                    listNamesOfStreams.Add(string.Format("{0} {1} {2} {3}", item.name(), item.type(), item.hostname(), item.nominal_srate()));
+                }
+
+                streamLookUpResult = listNamesOfStreams.Count + nStreamsFound;
             }
 
-            streamLookUpResult = listNamesOfStreams.Count + nStreamsFound;
         }
-
     }
+
 }
