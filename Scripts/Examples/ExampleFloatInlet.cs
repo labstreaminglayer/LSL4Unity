73177e86febfb5b1d7a07e54150660ba29602844
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-07-22 10:40:33 +0200

    Add abstract inlet implementations and an example script for a primitive float inlet

---
+++ Scripts/Examples/ExampleFloatInlet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using Assets.LSL4Unity.Scripts.AbstractInlets;
+
+namespace Assets.LSL4Unity.Scripts.Examples
+{
+    /// <summary>
+    /// Just an example implementation for a Inlet recieving float values
+    /// </summary>
+    public class ExampleFloatInlet : AFloatInlet
+    {
+        public string lastSample = String.Empty;
+
+        protected override void Process(float[] newSample, double timeStamp)
+        {
+            // just as an example, make a string out of all channel values of this sample
+            lastSample = string.Join(" ", newSample.Select(c => c.ToString()).ToArray());
+
+            Debug.Log(
+                string.Format("Got {0} samples at {1}", newSample.Length, timeStamp)
+                );
+        }
+    }
+}
