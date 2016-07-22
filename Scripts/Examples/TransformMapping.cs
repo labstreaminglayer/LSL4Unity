6beadf510dfb7af90696d678ea464ce0aabff76e
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-07-22 11:19:51 +0200

    Add an example for transform mapping used in the documentation

---
+++ Scripts/Examples/TransformMapping.cs
@@ -0,0 +1,21 @@
+﻿using Assets.LSL4Unity.Scripts.AbstractInlets;
+using UnityEngine;
+
+public class TransformMapping : AFloatInlet
+{
+    public Transform targetTransform;
+
+    protected override void Process(float[] newSample, double timeStamp)
+    {
+        //Assuming that a sample contains at least 3 values for x,y,z
+        float x = newSample[0];
+        float y = newSample[1];
+        float z = newSample[2];
+
+        // we map the coordinates to a rotation
+        var targetRotation = Quaternion.Euler(x, y, z);
+
+        // apply the rotation to the target transform
+        targetTransform.rotation = targetRotation;
+    }
+}
