df6b98765f860f131cf1f7fa64b286323a7743d4
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-08-19 10:25:06 +0200

    Extend the Demo Scene with a Marker Stream
    a random marker stream wrote"A" or "B" every second to the marker stream

---
+++ Demos/RandomMarker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+using System;
+
+// Don't forget the Namespace import
+using Assets.LSL4Unity.Scripts;
+
+public class RandomMarker : MonoBehaviour {
+
+	public LSLMarkerStream markerStream;
+	
+	void Start () {
+
+		Assert.IsNotNull(markerStream, "You forgot to assign the reference to a marker stream implementation!");
+
+		if (markerStream != null)
+			StartCoroutine(WriteContinouslyMarkerEachSecond());
+	}
+	
+	IEnumerator WriteContinouslyMarkerEachSecond()
+	{
+		while (true)
+		{
+			// an example for demonstrating the usage of marker stream
+			var currentMarker = GetARandomMarker();
+			markerStream.Write(currentMarker);
+			yield return new WaitForSecondsRealtime(1f);
+		}
+	}
+
+	private string GetARandomMarker()
+	{
+		return UnityEngine.Random.value > 0.5 ? "A" : "B";
+	}
+}
