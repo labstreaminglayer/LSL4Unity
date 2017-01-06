10342c4ebc9849c72d98b70818aa380d8308c311
Markus Fleck <fleckmarkus.privat@gmail.com>
2017-01-06 22:02:32 +0100

    Add first implementation

---
+++ Scripts/AsyncSampleAquisition/BaseJob.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+using LSL;
+using System;
+
+namespace Assets.LSL4Unity.Scripts.AsyncSampleAquisition
+{
+    /// <summary>
+    /// Abstraction for asynch thread based sample aquisition
+    /// </summary>
+    /// <typeparam name="T">Sample Type</typeparam>
+    /// <typeparam name="C">Cache implementation (Arrays, Collections etc.)</typeparam>
+    public abstract class ABaseJob<T, C> where C : class, IEnumerable<T[]>, new()
+    {
+
+        private object lockObject = new object();
+        private Thread thread;
+
+        protected liblsl.StreamInlet inlet;
+        protected C cache;
+
+        protected double timeOut = 0.0;
+
+        public bool HasData
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    return cache.Any();
+                }
+            }
+        }
+
+        public void GetData(out C cacheContent)
+        {
+            lock (lockObject)
+            {
+                cacheContent = cache.ToArray() as C;
+                cache = new C();
+            }
+        }
+
+        public void Start(liblsl.StreamInlet inlet, double aquisitionTimeOut = 0.0)
+        {
+            this.inlet = inlet;
+
+            this.timeOut = aquisitionTimeOut;
+
+            thread = new Thread(new ThreadStart(pullAsync));
+
+            thread.Start();
+        }
+
+        protected abstract void Initialize();
+
+        protected abstract void pullAsync();
+
+    }
+}
---
+++ Scripts/AsyncSampleAquisition/Jobs.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Assets.LSL4Unity.Scripts.AsyncSampleAquisition
+{
+    public abstract class BaseSampleJob<T> : ABaseJob<T, Queue<T[]>>
+    {
+        protected T[] tempSample;
+        private int expectedSampleCount = 0;
+
+        protected override void Initialize()
+        {
+            expectedSampleCount = inlet.info().channel_count();
+
+            if (cache == null)
+                cache = new Queue<T[]>();
+
+            tempSample = new T[expectedSampleCount];
+        }
+
+    }
+
+
+    public abstract class BaseChunkJob<T> : ABaseJob<T, Queue<T[]>>
+    {
+        protected T[] tempSample;
+        private int expectedSampleCount = 0;
+        private T[,] sampleBuffer;
+        private double[] timeStampBuffer;
+
+        protected override void Initialize()
+        {
+            expectedSampleCount = inlet.info().channel_count();
+
+            if (cache == null)
+                cache = new Queue<T[]>();
+            
+            tempSample = new T[expectedSampleCount];
+        }
+
+    }
+
+
+    public class FloatSampleJob : BaseSampleJob<float>
+    {
+        protected override void pullAsync()
+        {
+            if (inlet.samples_available() > 0)
+            {
+                double ts = 0.0;
+                do
+                {
+                    ts = inlet.pull_sample(tempSample);
+                    cache.Enqueue(tempSample);
+
+                } while (ts != 0.0);
+            }
+
+        }
+    }
+}
